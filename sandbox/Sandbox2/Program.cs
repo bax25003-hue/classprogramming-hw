5942776f3b5678302f143a4a0bf9d4fd556e0ea3
JJcoms0 <bax25003@byui.edu>
2026-04-28 08:53:18 -0600

    04/27 In-class work

---
+++ sandbox/Sandbox2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Sandbox2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Goodbye, Moon!");
+        // This is a comment
+        /*
+        The command for creating a new project is:
+        dotnet new console -o [name] --use-program-main
+        */
+        for (int i = 1; i < 10; i += 2)
+        {
+            Console.WriteLine($"Funny line {i}");
+        }
+    }
+}
