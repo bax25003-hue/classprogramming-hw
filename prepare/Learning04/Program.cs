863d1d89ce9bb16fcae9c2a2e099e8bd54c1e5a7
JJcoms0 <bax25003@byui.edu>
2026-06-01 12:35:24 -0600

    Inheritance Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Jace Baxter", "Kinetic and Potential Energy");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Jace Baxter", "Fractions", "7.3", "7-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Jace Baxter", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
